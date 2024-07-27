5d9fe44e7044a9d99ca5a0f9466e3f115855afc7
kitty-panics <e222d29f@outlook.com>
2024-07-27 08:31:36 -0400

    feat: Enable UPnP.

--- core-scripts/4.cross-compiling.sh
+++ core-scripts/4.cross-compiling.sh
@@ -8,7 +8,7 @@
 # - https://github.com/hamishcoleman/n3n/blob/main/doc/BuildConfig.md
 # - https://github.com/ntop/n2n/issues/1110
 
-# Set NDK Env
+# Set NDK ENV
 ENV_NDK_PATH="$(which env-droid-ndk)"
 source "$ENV_NDK_PATH"
 
@@ -16,6 +16,13 @@ source "$ENV_NDK_PATH"
 mkdir -p ./n2n/build; cd ./n2n
 ./autogen.sh
 ./configure --host=$TARGET
+#./configure CFLAGS="-O3" --host=$TARGET \
+#            --enable-natpmp \
+#            --enable-miniupnp
+#            --with-zstd \
+#            --with-openssl \
+#            --enable-cap \
+#            --enable-pcap \
 
 ## Configuration at Build time
 #    # lib-zstd
@@ -38,16 +45,17 @@ mkdir -p ./n2n/build; cd ./n2n
 #echo 'LDLIBS_EXTRA+=-lpcap' >>config.mak
 #echo 'CFLAGS+=-I$(realpath ../../thirdparty/build.4.pcap/)' >>config.mak
 #echo 'LDFLAGS+=-L$(realpath ../../thirdparty/build.4.pcap/)' >>config.mak
-#    # lib-natpmp
-#echo "#define HAVE_NATPMP 1" >>include/config.h
-#echo 'LDLIBS_EXTRA+=-lnatpmp' >>config.mak
-#echo 'CFLAGS+=-I$(realpath ../../thirdparty/build.5.natpmp/)' >>config.mak
-#echo 'LDFLAGS+=-L$(realpath ../../thirdparty/build.5.natpmp/)' >>config.mak
-#    # lib-miniupnp
+    # libnatpmp
+echo "#define HAVE_NATPMP 1" >>include/config.h
+echo 'LDLIBS_EXTRA+=-lnatpmp' >>config.mak
+echo 'CFLAGS+=-I$(realpath ../../thirdparty/build.libnatpmp/)' >>config.mak
+echo 'LDFLAGS+=-L$(realpath ../../thirdparty/build.libnatpmp/)' >>config.mak
+    # miniupnp
 #echo "#define HAVE_MINIUPNP 1" >>include/config.h
 #echo 'LDLIBS_EXTRA+=-lminiupnpc' >>config.mak
-#echo 'CFLAGS+=-I$(realpath ../../thirdparty/build.6.miniupnp/miniupnpc/build/)' >>config.mak
-#echo 'LDFLAGS+=-L$(realpath ../../thirdparty/build.6.miniupnp/miniupnpc/build/)' >>config.mak
+#echo 'CFLAGS+=-I$(realpath ../../thirdparty/build.miniupnp/miniupnpc/build/)' >>config.mak
+#echo 'LDFLAGS+=-L$(realpath ../../thirdparty/build.miniupnp/miniupnpc/build/)' >>config.mak
 
 # Build and Install
+mkdir -p ./build
 DESTDIR=./build make -j32 install
--- core-scripts/5.update-release-assets.sh
+++ core-scripts/5.update-release-assets.sh
@@ -4,8 +4,13 @@
 # Install
 #
 
+# Delete old version
 rm -rf ../core/bin
 rm -rf ../core/share
 
+# Install new version
 cp -rf ./n2n/build/usr/bin   ../core/
 cp -rf ./n2n/build/usr/share ../core/
+
+# Clean
+rm -rf ./n2n
